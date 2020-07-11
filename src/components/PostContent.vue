8c29f63db37eabd9f3fde4b4691f6b2734b7cbd5
hisatodayo <hisato621@icloud.com>
2020-07-11 20:08:31 +0900

    投稿画面途中

---
+++ src/components/PostContent.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div.wrap
+    PostContentTalk(
+      v-for="item in 5" :key="item.id"
+      )
+</template>
+
+<script>
+  import PostContentTalk from '@/components/PostContentTalk.vue'
+
+  export default {
+    name: 'PostContent',
+    components: {
+      PostContentTalk
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .wrap {
+    width: 95%;
+    margin: 10px auto 0;
+    background: #fff;
+    border-radius: 6px;
+    padding: 10px;
+  }
+</style>
