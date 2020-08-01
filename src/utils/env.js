129f80c57ea46ee6ddfc6ef1f61c47087ba1feac
MihiroHashimoto <mihiro.Hashimoto.1125@gmail.com>
2020-08-01 12:48:25 +0900

    Add: ローカルサーバーでテストサーバーのAPIを使用する際はログイン認証を外した

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,8 +1,10 @@
+const NODE_ENV = process.env.NODE_ENV
 const API_URL = process.env.VUE_APP_API_URL
 const PUSHER_APP_KEY = process.env.VUE_APP_PUSHER_APP_KEY
 const PUSHER_APP_CLUSTER = process.env.VUE_APP_PUSHER_APP_CLUSTER
 
 export default {
+  NODE_ENV,
   API_URL,
   PUSHER_APP_KEY,
   PUSHER_APP_CLUSTER
